6fdd20158d07c6f11a750a037bc91fd41bd54ac7
rfibigr <rfibigr@student.42.fr>
2022-11-27 13:00:30 +0100

    Add clientid and secret in settings

--- signal_bot/backend/core/config.py
+++ signal_bot/backend/core/config.py
@@ -8,6 +8,8 @@ class GoogleSettings(BaseSettings):
     #needs cleansing
     CALLBACK_URL: str = "http://127.0.0.1:8000/api/v1/auth/callback"
     AUTH_ANTIFORGERY_FILE: str = "signal_bot/backend/secrets/auth_antiforgery_tokens.json"
+    CLIENT_ID: str = "client_id"
+    CLIENT_SECRET: str = "client_secret"
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
--- signal_bot/backend/core/security.py
+++ signal_bot/backend/core/security.py
@@ -43,3 +43,18 @@ def is_id_token_valid(token : str):
 def get_auth_user(authorization: str = Header()):
     header_shredded = authorization.split("Bearer ")
     is_id_token_valid(header_shredded[1])
+
+def get_file_secret():
+    return {
+	    "web": {
+		    "client_id": settings.GOOGLE.CLIENT_ID,
+		    "project_id": "signal-bot-368420",
+		    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+		    "token_uri": "https://oauth2.googleapis.com/token",
+		    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+		    "client_secret": settings.GOOGLE.CLIENT_SECRET,
+		    "redirect_uris": [
+			    "http://127.0.0.1:8000"
+		    ]
+	    }
+    }
