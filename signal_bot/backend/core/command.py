15e7e9dc0a7b1c7b894f36908b85bb95d6902f89
rfibigr <rfibigr@student.42.fr>
2022-12-24 17:36:15 +0100

    Fix date check

--- signal_bot/backend/core/command.py
+++ signal_bot/backend/core/command.py
@@ -127,9 +127,9 @@ class Command:
             begin_time = time(*map(int, begin_time.split(":")))
             end_time = time(*map(int, end_time.split(":")))
             check_time = check_time or datetime.utcnow().time()
-            if begin_time < end_time:
+            if end_time > begin_time:
                 return begin_time <= check_time <= end_time
-            return begin_time >= check_time >= end_time
+            return check_time >= begin_time or check_time <= end_time
 
         if condition is None:
             return True
